536ed2f7fde70d2b52c990347368e3d63b667859
Arcanjo07 <adriano.patricio@dce.ufpb.br>
2014-07-30 10:52:56 -0300

    Task 1.03 finished for OO

---
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/CoffeeMachineException.java
@@ -0,0 +1,9 @@
+package br.ufpb.dce.aps.coffeemachine;
+
+@SuppressWarnings("serial")
+public class CoffeeMachineException extends Exception {
+	public CoffeeMachineException(String msg){
+		super(msg);
+	}
+	
+}
--- src/test/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -6,6 +6,7 @@ import java.util.List;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
@@ -13,20 +14,28 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	ComponentsFactory factory;
 	private int total, indice;
 	private Coin coin;
-	
+
 	public MyCoffeeMachine(ComponentsFactory factory) 
 	{
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin coin) 
+	public void insertCoin(Coin	 coin)
 	{
-		total += coin.getValue();
-		coins.add(coin);
-		indice ++;
-		this.factory.getDisplay().info(
-				"Total: US$ " + this.total / 100 + "." + this.total % 100);
+		try{
+			if(coin==null){
+				throw new CoffeeMachineException("Moeda inválida");
+			}
+			total += coin.getValue();
+			coins.add(coin);
+			/*indice ++;*/
+			this.factory.getDisplay().info(
+					"Total: US$ " + this.total / 100 + "." + this.total % 100);
+		}catch(CoffeeMachineException e){
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
