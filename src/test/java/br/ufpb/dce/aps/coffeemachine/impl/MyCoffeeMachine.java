5b3df38380ab6de7120ea4e179eef8baf285b0c0
Arcanjo07 <adriano.patricio@dce.ufpb.br>
2014-07-30 10:20:48 -0300

    Task 1.02 finished for OO

--- src/test/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,16 +1,32 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
+	List<Coin> coins = new ArrayList<Coin>();
 	ComponentsFactory factory;
+	private int total, indice;
+	private Coin coin;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) 
 	{
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin coin) 
+	{
+		total += coin.getValue();
+		coins.add(coin);
+		indice ++;
+		this.factory.getDisplay().info(
+				"Total: US$ " + this.total / 100 + "." + this.total % 100);
 	}
 
 }
